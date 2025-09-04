5735f1dfdeec7bf1db56b4752740c9625aea237e
KikeGonRam <kikeramirez160418@gmail.com>
2025-09-04 10:19:56 -0600

    MEJORAR EL MODAL DE EXPORTACION 3

--- src/components/modals/ExportModal.tsx
+++ src/components/modals/ExportModal.tsx
@@ -36,7 +36,7 @@ export const ExportModal: React.FC<ExportModalProps> = ({
       setTimeout(() => {
         setDownloadingFormat(null);
       }, 2000);
-    } catch (error) {
+    } catch {
       setDownloadingFormat(null);
     }
   };
